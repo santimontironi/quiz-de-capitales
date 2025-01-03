ce513a15a31aef0be041b7beedf4952467f43d35
santimontironi <santiimontironi@gmail.com>
2025-01-03 18:03:55 -0300

    Se creo el JSON y se realizo el fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './assets/css/app.css'
+import { useEffect, useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
 
+  const[preguntas,setPreguntas] = useState([])
+  const[preguntaActual,setPreguntaActual] = useState(0)
+
+  useEffect(() => {
+    fetch('preguntas.json')
+    .then(response => response.json())
+    .then(data => mezclarPreguntas(data))
+    .catch(error => console.error("Error al cargar el JSON: ", error))
+  },[])
+
+  const mezclarPreguntas = (data) => {
+    const preguntasMezcladas = data.sort(() => Math.random() - 0.5)
+    setPreguntas(preguntasMezcladas)
+  }
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="contenedor">
+      <h1>Quiz de capitales</h1>
+      {preguntas.length > 0 && ( //esta validacion es para que no se muestre nada hasta que se cargue el JSON previamente
+        <div className="pregunta">
+          <p>¿Cuál es la capital de {preguntas[preguntaActual].pais}?</p>
+        </div>
+      )}
+    </div>
   )
 }
 
